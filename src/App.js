3ccaf2b4d1e801875b88eaf2933c7991f9d4ae7d
Daniel Armas <daniel62armas@gmail.com>
2021-01-21 14:36:46 -0600

    Knowing props deeply

--- src/App.js
+++ src/App.js
@@ -5,13 +5,30 @@ import Welcome from './components/Welcome';
 import Greet from './components/Greet';
 //Para traer un componente que no está exportado de forma default, lo ponemos entre llaves
 function App() {
+  //const userName = 'Daniel Armas';
+  //const age = 19;
+  const user = {
+    name : 'Daniel Armas',
+    age : 18,
+    color: '#33FF2E'
+  };
+  const user2 = {
+    name : 'Sandra Armas',
+    age: 21,
+    color: '#E92EFF'
+  };
+
+  const greetingUser = (name) => {
+    alert(`Hello ${name}`);
+  }
+
   return (
     <div className="App">
       <header className="App-header">
         <Welcome />
         <img src={logo} className="App-logo" alt="logo" />
-        <Greet name = 'Daniel Armas' age='18' />
-        <Greet name = 'Sandra' age='21' />
+        <Greet userInfo = {user} greetUser={greetingUser}/>
+        <Greet userInfo = {user2} greetUser={greetingUser}/>
       </header>
     </div>
   );
--- src/components/Greet.js
+++ src/components/Greet.js
@@ -1,11 +1,24 @@
+import styled from 'styled-components';
 
+const ButtonGreet = styled.button`
+    border-radius: 10px;
+    color: #fff;
+    width: 10vw;
+    height: 8vh;
+    border: none;
+    font-size: 1em;
+    cursor: pointer;
+    font-weight: bold;
+`;
 
-const Greet = ({name, age}) => {
+const Greet = ({userInfo, greetUser}) => {
+    const {name, color, age} = userInfo;
     return (
         <div>
-            <h2>Hello {name}, you are {age} years old</h2>
+            <h3 style={{color: `${color}`}}>Hello {name}, you are {age} years old</h3>
+            <ButtonGreet onClick={() => greetUser(name)} style={{background: `${color}`}}>Greet</ButtonGreet>
         </div>
     )
-}
+};
 
 export default Greet;
